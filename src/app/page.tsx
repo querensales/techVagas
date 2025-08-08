38972f4c089d9ab1b2f9e288fa5db50a696b9b9c
Queren Sales <querenhsales@gmail.com>
2025-08-08 18:40:16 -0300

    chore: integra o componente JobList na página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 import { useState } from 'react';
 import Header from './components/Header/header';
+<<<<<<< Updated upstream
 import JobModal from './components/Header/JobModal/JobModal';
+=======
+import JobModal from './components/JobModal/JobModal';
+import JobList from './components/JobList/JobList'; 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 
 
 export default function Home() {
@@ -16,14 +24,12 @@ export default function Home() {
 
   return (
     <div className="bg-slate-50 min-h-screen">
-      {/* O Header recebe a função para abrir o modal como prop */}
       <Header onAnnounceClick={handleOpenModal} />
       
       <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        {/* Aqui você adicionará o conteúdo principal, como a lista de vagas */}
         <div className="px-4 py-6 sm:px-0">
-          <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 flex justify-center items-center">
-            <p className="text-slate-500">O feed de vagas aparecerá aqui.</p>
+          <div className=" h-96 flex justify-center items-center">
+            <JobList />
           </div>
         </div>
       </main>
