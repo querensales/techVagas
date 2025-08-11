fe48cb63d48ab20897561ab6043e91e67defd53e
Queren Sales <querenhsales@gmail.com>
2025-08-11 10:55:31 -0300

    🚧 Filtros de busca
    criando labels e inputs dos filtros de busca

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header/header';
 import JobModal from './components/Header/JobModal/JobModal';
 import JobList from './components/JobList/JobList';
+import FilterSidebar from './components/FilterSideBar/FilterSidebar';
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -20,8 +21,7 @@ export default function Home() {
       <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="gap-8 flex">
           <div className="w-1/4">
-            <h2 className="text-xl font-bold text-slate-800">Filtros</h2>
-            <p>Aqui vai ficar os filtros</p>
+            <FilterSidebar />
           </div>
           <div className="w-3/4"><JobList /></div>
         </div>
