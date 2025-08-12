ba177130bc19fba461376cc886a59c7d406d6fad
Queren Sales <querenhsales@gmail.com>
2025-08-12 15:49:02 -0300

    💄 Vagas em colunas
    colocando flex col nas vagas

--- src/app/components/JobList/JobList.tsx
+++ src/app/components/JobList/JobList.tsx
@@ -12,7 +12,7 @@ export default function JobList({ jobs }: JobListProps) {
   return (
     <> 
       {jobs.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="flex flex-col gap-6">
           {jobs.map(job => (
             <JobCard key={job.id} job={job} />
           ))}
